9816a0b142ee9274f4bc3645a484ae8d5b6c2d35
= <=>
2021-08-13 15:57:27 -0500

    5px margin top

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ const HomePage = () => {
         section {
           text-align: center;
           margin-bottom: 2rem;
-          margin-top: 10px;
+          margin-top: 5px;
         }
       `}</style>
     </Layout>
